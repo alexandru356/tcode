1a694d26d4c05495f312a79708c0822aeb7c91d4
Alexandru Ciuca <a.ciuca@hotmail.com>
2026-03-14 17:28:11 -0500

    Dev (#7)
    * base home page + nav to search
    
    * imported problem sets and started session.py
    
    * comment delete
    
    ---------
    
    Co-authored-by: Andrei Cretu2 <andrei.cretu4676@gmail.com>

--- src/tcode/cli.py
+++ src/tcode/cli.py
@@ -9,8 +9,8 @@ def cli() -> None:
 @click.option("--file", "-f", required=True, type=click.Path(), help="Path to your solution file")
 def start(file: str) -> None:
     from tcode.tui import TCodeApp
+    from tcode.config import SessionConfig
     TCodeApp().run()
 
-# Guard to prevent imports used in other files to execute the whole cli
 if __name__ == "__main__":
     cli()
